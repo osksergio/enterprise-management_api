6b23ebde0d24e9a0e7623ec611e067bcefeb96f3
Sergio Oseko <osk.sergio@gmail.com>
2023-01-23 22:21:54 -0300

    feat: criado índice com valor único (email)

--- db/migrate/20230116090107_create_companies.rb
+++ db/migrate/20230116090107_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[6.1]
   def change
     create_table :companies do |t|
       t.string :description
-      t.string :email
+      t.string :email, index: { unique: true, name: 'unique_emails' }
       t.string :password
 
       t.timestamps
