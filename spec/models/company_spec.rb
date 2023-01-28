f72262dfd962e7406e4b31a775056b302e47802a
Sergio Oseko <osk.sergio@gmail.com>
2023-01-28 00:10:25 -0300

    test: implementados os testes do model (company)

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Create Company' do
+    new_company = Company.create(description: "Description", email: "foo@bar.com", password: "password")
+    expect(new_company.description).to eq("Description")
+  end
+
+  describe 'Required validations' do
+    it { should validate_presence_of(:description) }
+    it { should validate_uniqueness_of(:email) }
+  end
+
+  it 'Delete Company' do
+    new_company = Company.create(description: "Description", email: "foo@bar.com", password: "password")
+    new_company.destroy
+    expect{ new_company.reload }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
