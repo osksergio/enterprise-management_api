c7a399f8421f431974eb7d693984519a49ce9aea
Sergio Oseko <osk.sergio@gmail.com>
2023-01-31 21:53:33 -0300

    refactor: ajustes nos testes após refatorar o nome do service object (create company)

--- spec/services/company/create_company_spec.rb
+++ spec/services/company/create_company_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Company::Create do
+RSpec.describe Company::CreateCompany do
   let(:params) {
     {
       description: "Silva & Silva Company Inc.",
@@ -11,14 +11,14 @@ RSpec.describe Company::Create do
 
   context "When created company with valid parameters" do
     it "return  true" do
-      service = Company::Create.call(params)
+      service = Company::CreateCompany.call(params)
       expect(service).to be_truthy
     end
   end
 
   context "When created company with invalid parameters" do
     it "return nil" do
-      service = Company::Create.call({description: ""})
+      service = Company::CreateCompany.call({description: ""})
       expect(service).to be_nil
     end
   end
