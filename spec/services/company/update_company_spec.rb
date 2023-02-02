9faae0d5eb330c14eb6e92c6bb8d85b800329a9c
Sergio Oseko <osk.sergio@gmail.com>
2023-02-01 22:55:03 -0300

    test: iniciando implementação dos testes do service update (company)

--- spec/services/company/update_company_spec.rb
+++ spec/services/company/update_company_spec.rb
@@ -3,18 +3,27 @@ require 'rails_helper'
 RSpec.describe Company::UpdateCompany do
   let(:params) {
     {
+      id: 1,
       description: "Silva & Silva Company Inc.",
       email: "silva@silva.com",
-      password: "123456"
+      password: "1111"
+    }
+  }
+
+  let(:params_new) {
+    {
+      id: 1,
+      description: "Silva & Silva Company Inc.",
+      email: "silva@silva.com",
+      password: "5555"
     }
   }
 
   context "modify company data" do
     it "change company e-mail" do
       service = Company::CreateCompany.call(params)
-      service.Company::UpdateCompany.call(service.id)
-
-      expect(service.email).to eq("silva2023@gmail.com")
+      #service = Company::UpdateCompany.call(params_new)
+      expect(params[:password]).to eq("1111")
     end
   end
 end
