419eaed21a2f8e70e5023bc6a89db8a21a8011c7
Sergio Oseko <osk.sergio@gmail.com>
2023-02-03 15:21:13 -0300

    test: service update test implemented (company)

--- spec/services/company/update_company_spec.rb
+++ spec/services/company/update_company_spec.rb
@@ -1,29 +1,28 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Company::UpdateCompany do
+  let(:company) { Company.create(description: 'old_description', email: 'old_email', password: 'old_password') }
+
   let(:params) {
     {
-      id: 1,
-      description: "Silva & Silva Company Inc.",
-      email: "silva@silva.com",
-      password: "1111"
+      id: company.id,
+      description: 'Silva & Silva Company Inc.',
+      email: 'novo_email@silva.com',
+      password: '5555'
     }
   }
 
-  let(:params_new) {
-    {
-      id: 1,
-      description: "Silva & Silva Company Inc.",
-      email: "silva@silva.com",
-      password: "5555"
-    }
-  }
+  context 'modify company data' do
+    it 'change company e-mail' do
+      service = Company::UpdateCompany.call(params)
+      company.reload
 
-  context "modify company data" do
-    it "change company e-mail" do
-      service = Company::CreateCompany.call(params)
-      #service = Company::UpdateCompany.call(params_new)
-      expect(params[:password]).to eq("1111")
+      expect(service).to be_truthy
+      expect(company.description).to eq('Silva & Silva Company Inc.')
+      expect(company.email).to eq('novo_email@silva.com')
+      expect(company.password).to eq('5555')
     end
   end
 end
