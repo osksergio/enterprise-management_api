06178952fed2adf22a79abfb215c4909fc2b53c1
Sergio Oseko <osk.sergio@gmail.com>
2023-02-04 10:17:44 -0300

    feat: implementados os testes de requests (update, index, show company)

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -21,6 +21,27 @@ RSpec.describe "/companies", type: :request do
     {}
   }
 
+  describe "GET /index" do
+    it "renders a successful response" do
+      Company.create! valid_attributes
+      get companies_url, headers: valid_headers, as: :json
+      expect(response).to be_successful
+    end
+
+    it "returns http success" do
+      get companies_url, headers: valid_headers, as: :json
+      expect(response.status).to eq 200
+    end
+  end
+
+  describe "GET /show" do
+    it "renders a successful response" do
+      company = Company.create! valid_attributes
+      get company_url(company), as: :json
+      expect(response).to be_successful
+    end
+  end
+
   describe "POST /create" do
     context "with valid parameters" do
       it "creates a new Company" do
@@ -51,6 +72,44 @@ RSpec.describe "/companies", type: :request do
              params: { company: invalid_attributes }, headers: valid_headers, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to match(a_string_including("application/json"))
+      end
+    end
+  end
+
+  describe "PATCH /update_company" do
+    context "with valid parameters" do
+      let!(:company) {
+        Company.create(
+          description: 'Original Description Company',
+          email: 'email@email.com',
+          password: 'password')
+      }
+
+      it 'valid company attributes' do
+        patch company_url(company), params:
+          {
+            description: 'New Description Company',
+            email: 'new_email@email.com',
+            password: 'new_password'
+          }, headers: valid_headers, as: :json
+
+        expect(response.status).to eq(200)
+        expect(company.reload.description).to eq('New Description Company')
+        expect(company.reload.email).to eq('new_email@email.com')
+        expect(company.reload.password).to eq('new_password')
+      end
+
+      it 'invalid company attributes' do
+        patch company_url(company), params: {
+          description: nil,
+          email: '',
+          password: '123456'
+        }, headers: valid_headers, as: :json
+
+        expect(response.status).to eq(422)
+        expect(company.reload.description).to eq('Original Description Company')
+        expect(company.reload.email).to eq('email@email.com')
+        expect(company.reload.password).to eq('password')
       end
     end
   end
