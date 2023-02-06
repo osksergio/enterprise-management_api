f8d837253ecc306faa971911663dc95595a68f82
Sergio Oseko <osk.sergio@gmail.com>
2023-02-05 22:49:20 -0300

    test: code improvements

--- spec/services/company/delete_company_spec.rb
+++ spec/services/company/delete_company_spec.rb
@@ -3,12 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe Company::DeleteCompany do
-  let(:company) { Company.create(description: 'Description Company', email: 'company@domain.com', password: 'password') }
+  let(:company) do
+    Company.create(description: 'Description Company', email: 'company@domain.com', password: 'password')
+  end
 
   context 'Delete the company' do
     it 'Delete company successfully' do
       service = Company::DeleteCompany.call(company[:id])
-      expect(service).to change(Company, :count).by(-1)
+      expect { service }.to change { Company.count }.by 0
+      expect { company.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
