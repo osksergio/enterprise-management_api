618607a52fec96c93af0e42992b980fe07911bef
Sergio Oseko <osk.sergio@gmail.com>
2023-02-08 22:36:58 -0300

    refactor: implementadas várias melhorias das services e método call

--- app/services/callable.rb
+++ app/services/callable.rb
@@ -2,7 +2,32 @@ module Callable
   extend ActiveSupport::Concern
   class_methods do
     def call(*args)
-      new(*args).call
+      service = new(*args)
+      service.call
+      service
+    end
+  end
+
+  def success?
+    errors.nil?
+  end
+
+  def errors
+    @errors
+  end
+
+  def t(key)
+    I18n.t(key, scope: self.class.name.underscore.split("/"))
+  end
+
+  def add_error(object, attribute = nil, type = nil)
+    @errors ||= []
+    if object.present?
+      object.errors.each do |err|
+        @errors << { attribute: err.attribute, message: t(err.type) }
+      end
+    else
+      @errors << { attribute: attribute, type: t(type) }
     end
   end
 end
