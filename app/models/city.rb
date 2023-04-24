497436b76a44b6988a004b61d2d125bf063146fc
Sergio Oseko <osk.sergio@gmail.com>
2023-04-24 19:50:19 -0300

    feat: implementadas validações

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,3 +1,8 @@
+# frozen_literal_string: true
+
+# entity City (Class)
 class City < ApplicationRecord
   belongs_to :company
+  validates :description, presence: true
+  validates :state, presence: true
 end
