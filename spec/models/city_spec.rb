3f855cf106ded2900fd109319f1558920d8271ac
Sergio Oseko <osk.sergio@gmail.com>
2023-04-24 17:10:50 -0300

    test: iniciando os testes do model city

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Create City' do
+  	new_city = City.create(description: 'Description', state: 'SP', company_id: "1")
+  	expect(new_city.description).to eq('Description')
+  end
 end
